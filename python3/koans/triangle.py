7b029ec46547964eb58212090c998922716459b1
kevgray83 <75575265+kevgray83@users.noreply.github.com>
2021-08-22 22:03:02 +0100

    Progress this weekend

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,7 +18,24 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    sides = sorted([a,b,c])
+    sides_sorted = sorted(set(sides))
+
+    if sides_sorted[0] == 0:
+        raise TriangleError("Sides cannot be 0")
+
+    if sides[2] >= sides[0] + sides[1]:
+        raise TriangleError("Sum of two sides cannot be greater than third")
+
+    if len(sides_sorted) == 1:
+         return "equilateral"
+    elif len(sides_sorted) == 2: 
+        return "isosceles"
+    else: return "scalene"
+
+
+    
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
